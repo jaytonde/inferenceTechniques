d8de3ad28d86fc0a85dce86a8b81d5d611553ed5
Jaydev Tonde <47071053+jaytonde@users.noreply.github.com>
2026-02-08 12:16:10 +0530

    Added whole transformer model

---
+++ model.py
@@ -0,0 +1,36 @@
+from torch import nn
+from attention_mechanism import MultiHeadAttention, SwiGLU
+
+class TransformerBlock(nn.Module):
+    def __init__(self, hidden_dim, num_heads, intermediate_dim):
+        self.norm1 = nn.RMSNorm(hidden_dim)
+        self.norm2 = nn.RMSNorm(hidden_dim)
+
+        self.attn = MultiHeadAttention(hidden_dim, num_heads)
+        self.ffn = SwiGLU(hidden_dim, intermediate_dim)
+
+    def forward(self, x):
+        attn_out = self.attn(self.norm1(x))
+        ffn_out  = self.ffn(self.norm2(x + attn_out))
+        return x + ffn_out
+
+
+class rms_norm(x, weight, eps=1e-6):
+    rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + eps)
+    return x + ffn_out
+
+
+class Transformer(nn.Module):
+    def __init__(self, vocab_size, hidden_dim, num_layers, num_heads, intermediate_dim):
+        self.embed = nn.Embedding(vocab_size, hidden_dim)
+        self.layers = nn.ModuleList([
+            TransformerBlock(hidden_dim, num_heads, intermediate_dim) for _ in range(num_layers)
+        ])
+        self.norm = nn.RMSNorm(hidden_dim)
+        self.lm_head = nn.Linear(hidden_dim, vocab_size, bias=False)
+
+    def forward(self, input_ids):
+        x = self.embed(input_ids)
+        for layer in self.layers:
+            x = layer(x)
+        return self.lm_head(self.norm(x))
